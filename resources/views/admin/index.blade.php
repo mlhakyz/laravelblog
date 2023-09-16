ef6583c7af2257b3a577f8ab9f7a5dd793717a25
Melih Akyuz <melihakyuz@Melihs-MacBook-Pro.local>
2023-09-16 09:23:36 +0300

    js files deleted

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -197,47 +197,11 @@
     <!-- ./wrapper -->
 
     <!-- jQuery -->
-    <script src="{{ URL::asset('plugins/jquery/jquery.min.js') }}"></script>
-    <!-- jQuery UI 1.11.4 -->
-    <script src="{{ URL::asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
-    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-    <script>
-        $.widget.bridge('uibutton', $.ui.button)
-    </script>
-    <!-- Bootstrap 4 -->
-    <script src="{{ URL::asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <!-- ChartJS -->
-    <script src="{{ URL::asset('plugins/chart.js/Chart.min.js') }}"></script>
-    <!-- Sparkline -->
-    <script src="{{ URL::asset('plugins/sparklines/sparkline.js') }}"></script>
-    <!-- JQVMap -->
-    <script src="{{ URL::asset('plugins/jqvmap/jquery.vmap.min.js') }}"></script>
-    <script src="{{ URL::asset('plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
-    <!-- jQuery Knob Chart -->
-    <script src="{{ URL::asset('plugins/jquery-knob/jquery.knob.min.js') }}"></script>
-    <!-- daterangepicker -->
-    <script src="{{ URL::asset('plugins/moment/moment.min.js') }}"></script>
-    <script src="{{ URL::asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
-    <!-- Tempusdominus Bootstrap 4 -->
-    <script src="{{ URL::asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
-    <!-- Summernote -->
-    <script src="{{ URL::asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
-    <!-- overlayScrollbars -->
-    <script src="{{ URL::asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{ URL::asset('js/adminlte.js') }}"></script>
-    <script src="{{ URL::asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ URL::asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-    <script src="{{ URL::asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ URL::asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ URL::asset('../jquery/jquery.min.js') }}"></script>
+    <script src="{{ URL::asset('../js/adminlte.js') }}"></script>
     <script src="{{ URL::asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
     <script src="{{ URL::asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
-    <script src="{{ URL::asset('plugins/jszip/jszip.min.js') }}"></script>
-    <script src="{{ URL::asset('plugins/pdfmake/pdfmake.min.js') }}"></script>
-    <script src="{{ URL::asset('plugins/pdfmake/vfs_fonts.js') }}"></script>
-    <script src="{{ URL::asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
-    <script src="{{ URL::asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
-    <script src="{{ URL::asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+
     <script>
         $(function() {
             $("#example1").DataTable({
